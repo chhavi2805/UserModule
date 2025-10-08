c8b0e3ddd42d9c64bf578a6ebcb7bd37d834d416
Saransh Gupta <saransh2907@outlook.com>
2025-10-08 21:03:42 +0530

    logging amd deploy

--- usermodule/src/main/java/com/example/usermodule/controller/UserController.java
+++ usermodule/src/main/java/com/example/usermodule/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.usermodule.controller;
 import com.example.usermodule.model.User;
 import com.example.usermodule.service.UserService;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
@@ -16,19 +18,24 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private static final Logger logger = LogManager.getLogger(UserController.class);
+
     @GetMapping("getAll")
     public List<User> getAllUsers(){
+        logger.info("Getting all users---------------------->");
+        System.out.println("Getting all users---------------------->");
         return userService.fetchAllUser();
     }
 
     @PostMapping("/addUser")
     public ResponseEntity<?> addNewUser(@Valid @RequestBody User user){
-        System.out.println("Adding new user" + user.toString() + "---------------------->");
+        logger.info("Adding new user" + user.toString() + "---------------------->");
         User userResponse = null;
         try {
             userResponse = userService.addNewUser(user);
             return ResponseEntity.ok(userResponse);
         } catch (Exception e) {
+            logger.error(e.getMessage());
             return ResponseEntity.status(409).body("User could not be added");
         }
 
